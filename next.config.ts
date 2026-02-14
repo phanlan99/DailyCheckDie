d115f946289d33f4281f74cb17896d226625e682
phanlan99 <phanlandt123@gmail.com>
2026-02-14 21:00:55 +0700

    tăng 4mb

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 1. Cấu hình cho phép load ảnh từ Cloudinary (Đã làm từ trước)
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
-        pathname: '**', // Cho phép mọi đường dẫn từ cloudinary
+        pathname: '**',
       },
     ],
+  },
+
+  // 2. THÊM ĐOẠN NÀY ĐỂ TĂNG GIỚI HẠN UPLOAD
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '4mb', // Tăng lên 4MB
+    },
   },
 };
 
